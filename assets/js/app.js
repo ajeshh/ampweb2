24118c9666eaaa86b9ed7650813e2f7c07a52ebe
Ajesh Shah <ajeshshah@gmail.com>
2015-04-16 14:48:34 -0700

    killed contact form transition

--- assets/js/app.js
+++ assets/js/app.js
@@ -177,13 +177,13 @@ if( $('#s-contact').length != 0 ) {
         $contact.find('~ label').hide();
     }
 
-    $contact.blur(function() {
-        if( $(this).val().length != 0 ) {
-            $(this).find('~ label').hide();
-        } else {
-           $(this).find('~ label').fadeIn(); 
-        }
-    });
+    // $contact.blur(function() {
+    //     if( $(this).val().length != 0 ) {
+    //         $(this).find('~ label').hide();
+    //     } else {
+    //        $(this).find('~ label').fadeIn(); 
+    //     }
+    // });
 
 }
 
